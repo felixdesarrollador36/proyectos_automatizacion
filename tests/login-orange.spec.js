759a6f82fd95d123d116664a49c40d82f1abf3d4
Luis Angel Garcia Louis <ludevgarcialouis@gmail.com>
2026-01-10 18:06:24 -0400

    automatizacion login - test v1

---
+++ tests/login-orange.spec.js
@@ -0,0 +1,13 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test("acceso login", async ({ page }) => {
+  await page.goto(
+    "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+  );
+
+  await page.locator('[name="username"]').fill("Admin");
+  await page.locator('[name="password"]').fill("admin123");
+
+  await page.locator('[type="submit"]').click();
+});
